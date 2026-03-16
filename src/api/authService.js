12b9a3cffe076dbbcfb0b417d5772292bdb5c915
jackan04 <jacob.asker@icloud.com>
2026-03-16 11:16:08 +0100

    fix: add passwordConfirmation to register method in the body

--- src/api/authService.js
+++ src/api/authService.js
@@ -3,13 +3,13 @@ class AuthService {
     this.baseUrl = "http://localhost:3000/api/auth";
   }
 
-  async register(username, password) {
+  async register(username, password, passwordConfirmation) {
     const response = await fetch(`${this.baseUrl}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify({ username, password, passwordConfirmation }),
     });
 
     return response.json();
