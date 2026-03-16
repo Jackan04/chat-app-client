b3920d75fbf8e6552f36da276afbf2ec42c65f03
jackan04 <jacob.asker@icloud.com>
2026-03-16 10:27:35 +0100

    feat: add AuthContext file

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ token, login, logout, isAuthenticated: !!token }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(AuthContext);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { AuthProvider, useAuth };
