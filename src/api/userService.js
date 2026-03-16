fef996ca0ab4930bdb8294d7ee9c4e9e6d30de9a
jackan04 <jacob.asker@icloud.com>
2026-03-16 18:18:50 +0100

    feat: implement getUserById method

--- src/api/userService.js
+++ src/api/userService.js
@@ -3,6 +3,23 @@ class UserService {
     this.baseUrl = "/api/users";
   }
 
+  async getUserById(token, id) {
+    const response = await fetch(`${this.baseUrl}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw Error(data.message);
+    }
+
+    return data;
+  }
+
   async getUsersByUsername(token, username) {
     const response = await fetch(`${this.baseUrl}?username=${username}`, {
       headers: {
