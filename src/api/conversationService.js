56b62db19ac8bb601f0719c451c4b1379c4486b2
jackan04 <jacob.asker@icloud.com>
2026-03-16 13:19:00 +0100

    feat: implement ConversationService class with getConversations method

---
+++ src/api/conversationService.js
@@ -0,0 +1,24 @@
+class ConversationService {
+  constructor() {
+    this.baseUrl = "/api/conversations";
+  }
+
+  async getConversations(token) {
+    const response = await fetch(this.baseUrl, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    return data;
+  }
+}
+
+export default ConversationService;
