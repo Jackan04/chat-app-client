e652e472d690f12213d75a793d6019a246a6a2e2
jackan04 <jacob.asker@icloud.com>
2026-03-16 11:10:15 +0100

    feat: add AuthService with register method

---
+++ src/api/authService.js
@@ -0,0 +1,19 @@
+class AuthService {
+  constructor() {
+    this.baseUrl = "http://localhost:3000/api/auth";
+  }
+
+  async register(username, password) {
+    const response = await fetch(`${this.baseUrl}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    return response.json();
+  }
+}
+
+export default AuthService;
