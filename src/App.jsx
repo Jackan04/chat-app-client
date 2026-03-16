485a83f535959ad1a92b9719efefa78bd7be91a3
jackan04 <jacob.asker@icloud.com>
2026-03-16 10:36:25 +0100

    feat: install and setup react router; add routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/login" element={<h2>Login</h2>} />
+        <Route path="/register" element={<h2>Register</h2>} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/conversations" element={<h2>Conversations</h2>} />
+          <Route path="/conversations/:id" element={<h2>Conversation</h2>} />
+          <Route
+            path="/conversations/new"
+            element={<h2>New Conversation</h2>}
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
