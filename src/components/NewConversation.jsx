6d851baec3b5f98d163cef8911d0773afdc72ffa
jackan04 <jacob.asker@icloud.com>
2026-03-16 18:31:09 +0100

    feat: add user dialog for displaying user details and initiating new conversations

--- src/components/NewConversation.jsx
+++ src/components/NewConversation.jsx
@@ -9,6 +9,7 @@ export default function NewConversation() {
   const [searchQuery, setSearchQuery] = useState("");
   const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
+  const [isOpen, setIsOpen] = useState(null);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -43,8 +44,36 @@ export default function NewConversation() {
       </header>
       <ul>
         {users.length > 0 &&
-          users.map((user) => <li key={user.id}>{user.username}</li>)}
+          users.map((user) => (
+            <div key={user.id}>
+              <li onClick={() => setIsOpen(user.id)}>{user.username}</li>
+              <UserDialog
+                user={user}
+                isOpen={isOpen}
+                handleClose={() => setIsOpen(null)}
+                handleNewConversation={""}
+              />
+            </div>
+          ))}
       </ul>
     </>
+  );
+}
+
+function UserDialog({ user, isOpen, handleClose, handleNewConversation }) {
+  return (
+    <dialog open={isOpen}>
+      <header>
+        <h2>{user.username}</h2>
+      </header>
+      <main>
+        <p>{user.online ? "Online" : "Offline"}</p>
+        <p>Bio: {user.bio}</p>
+        <button onClick={handleNewConversation}>New Conversation</button>
+      </main>
+      <footer>
+        <button onClick={handleClose}>Close</button>
+      </footer>
+    </dialog>
   );
 }
