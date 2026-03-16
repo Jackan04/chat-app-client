b263b61396b0f2443634f84f5a00e9d035ae6405
jackan04 <jacob.asker@icloud.com>
2026-03-16 18:53:37 +0100

    feat: implement createConversation method

--- src/api/conversationService.js
+++ src/api/conversationService.js
@@ -37,6 +37,28 @@ class ConversationService {
     return data;
   }
 
+  async createConversation(token, participants) {
+    const user1Id = participants[0].id;
+    const user2Id = participants[1].id;
+
+    const response = await fetch(`${this.baseUrl}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user1Id, user2Id }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+
+    return data;
+  }
+
   async sendMessage(token, conversationId, content) {
     const response = await fetch(`${this.baseUrl}/${conversationId}/messages`, {
       method: "POST",
