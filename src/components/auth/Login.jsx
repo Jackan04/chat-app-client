ef7316683f391c98319b1416946ae40cdda4d389
jackan04 <jacob.asker@icloud.com>
2026-03-16 12:49:31 +0100

    feat: implement handleSubmit function and add error handling

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,15 +1,39 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import AuthService from "../../api/authService";
+import { useAuth } from "../../context/useAuth";
+
+const authService = new AuthService();
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [validationErrors, setValidationErrors] = useState([]);
+  const { login } = useAuth();
 
-  function handleSubmit() {}
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setValidationErrors([]);
+    try {
+      const token = await authService.login(username, password);
+
+      login(token);
+    } catch (error) {
+      if (error.validationErrors && error.validationErrors.length > 0) {
+        setValidationErrors(error.validationErrors);
+      }
+    }
+  }
   return (
     <div>
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
+        <ul>
+          {validationErrors.length > 0 &&
+            validationErrors.map((error, index) => (
+              <li key={index}>{error.msg}</li>
+            ))}
+        </ul>
         <label htmlFor="username">
           Username
           <input
