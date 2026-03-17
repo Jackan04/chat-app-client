02da9b431dc5f1b3f4559198b359f72e450d6981
jackan04 <jacob.asker@icloud.com>
2026-03-17 14:37:38 +0100

    feat: implement handling of updating user profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import { useAuth } from "../context/useAuth";
+import UserService from "../api/userService";
+
+const userService = new UserService();
+
+export default function Profile() {
+  const { currentUser, token, loadCurrentUser } = useAuth();
+  const [isEditing, setIsEditing] = useState(false);
+  const [error, setError] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [bio, setBio] = useState("");
+  const [online, setOnline] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!currentUser) return;
+
+    try {
+      await userService.updateUser(token, currentUser.id, {
+        displayName,
+        bio,
+        online,
+      });
+      await loadCurrentUser();
+      setIsEditing(false);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  function handleEditStart() {
+    if (!currentUser) return;
+
+    setDisplayName(currentUser.displayName ?? "");
+    setBio(currentUser.bio ?? "");
+    setOnline(Boolean(currentUser.online));
+    setIsEditing(true);
+  }
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <>
+      <h2>Profile</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username
+          <input value={currentUser.username} disabled></input>
+        </label>
+        <label htmlFor="online">
+          Online
+          <input
+            id="online"
+            disabled={!isEditing}
+            type="checkbox"
+            checked={isEditing ? online : Boolean(currentUser.online)}
+            onChange={(event) => setOnline(event.target.checked)}
+          />
+        </label>
+
+        <label htmlFor="displayName">
+          Display Name
+          <input
+            value={isEditing ? displayName : (currentUser.displayName ?? "")}
+            disabled={!isEditing}
+            type="text"
+            id="displayName"
+            onChange={(event) => setDisplayName(event.target.value)}
+          />
+        </label>
+        <label htmlFor="bio">
+          Bio
+          <input
+            type="text"
+            id="bio"
+            disabled={!isEditing}
+            value={isEditing ? bio : (currentUser.bio ?? "")}
+            onChange={(event) => setBio(event.target.value)}
+          />
+        </label>
+
+        {!isEditing && (
+          <button type="button" onClick={handleEditStart}>
+            Edit
+          </button>
+        )}
+
+        {isEditing && (
+          <>
+            <button type="submit">Save</button>
+            <button type="button" onClick={() => setIsEditing(false)}>
+              Cancel
+            </button>
+          </>
+        )}
+      </form>
+    </>
+  );
+}
