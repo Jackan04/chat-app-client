41ed06bd460b7fd39eb047d07daab9b379911bbb
jackan04 <jacob.asker@icloud.com>
2026-03-17 18:58:39 +0100

    refactor: remove logout button from Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,24 +2,21 @@ import { NavLink, Link } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 
 export default function Nav() {
-  const { isAuthenticated, logout } = useAuth();
+  const { isAuthenticated } = useAuth();
   return (
     <nav className="flex justify-between items-center">
       <Link to="/">Chat App</Link>
       <ul className="flex items-center gap-2">
         {isAuthenticated && (
           <>
-            <li className="btn">
+            <li>
               <NavLink to="/">Conversations</NavLink>
             </li>
-            <li className="btn">
+            <li>
               <NavLink to="/conversations/new">New Conversation</NavLink>
             </li>
-            <li className="btn">
-              <NavLink to="/profile">Profile</NavLink>
-            </li>
             <li>
-              <button onClick={logout}>Logout</button>
+              <NavLink to="/profile">Profile</NavLink>
             </li>
           </>
         )}
