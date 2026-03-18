b66e7d30792e597758c806108202187b44af746a
jackan04 <jacob.asker@icloud.com>
2026-03-18 14:34:35 +0100

    refactor: update layout: replace label with arrow icon

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,8 +3,10 @@ import { useNavigate } from "react-router-dom";
 export default function PageHeader({ title }) {
   const navigate = useNavigate();
   return (
-    <div>
-      <button onClick={() => navigate(-1)}>Back</button>
+    <div className="hstack gap-4 mb-6 items-center">
+      <button className="outline" onClick={() => navigate(-1)}>
+        <i className="fa-solid fa-arrow-left"></i>
+      </button>
       <h2>{title}</h2>
     </div>
   );
