edb03bbb57cc7d6351ffe3863646d9d6269f5666
jackan04 <jacob.asker@icloud.com>
2026-03-19 08:32:40 +0100

    feat: add button to navigate back on error

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Error({ message }) {
+  const navigate = useNavigate();
   return (
-    <div className="error-message" role="alert" data-variant="error">
-      <strong>Error!</strong> <p>{message}</p>
+    <div className="error-message vstack">
+      <div role="alert" data-variant="error">
+        <strong>Error!</strong> <p>{message}</p>
+      </div>
+      <button className="outline" onClick={() => navigate(-1)}>
+        Go Back
+      </button>
     </div>
   );
 }
